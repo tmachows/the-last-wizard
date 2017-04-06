83fa6b39f0917715e19971b113d91fbb07850fb5
Jakub Janusz <Jakub Janusz>
2017-04-06 20:24:20 +0200

    Basic enemy spawning (3 spawn points).

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -3,27 +3,23 @@
 public class EnemyManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _Enemy;                // The enemy prefab to be spawned.
+    private GameObject _Enemy;
     [SerializeField]
-    private float s_SawnTime = 3f;            // How long between each spawn.
+    private float s_SawnTime = 3f;
     [SerializeField]
-    private Transform[] _SpawnPoints;         // An array of the spawn points this enemy can spawn from.
+    private Transform[] _SpawnPoints;
 
 
     void Start()
     {
-        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
         InvokeRepeating("Spawn", s_SawnTime, s_SawnTime);
     }
 
 
     void Spawn()
     {
-
-        // Find a random index between zero and one less than the number of spawn points.
         int spawnPointIndex = Random.Range(0, _SpawnPoints.Length);
-
-        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        
         Instantiate(_Enemy, _SpawnPoints[spawnPointIndex].position, _SpawnPoints[spawnPointIndex].rotation);
     }
 }
