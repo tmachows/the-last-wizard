0e23eae447391bf679471cc7f4ce46c3cd4950e5
Tymoteusz Machowski <tmachows@gmail.com>
2017-04-20 19:17:23 +0200

    basic enemies movement

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMover : MonoBehaviour {
+
+    private Vector3 _WizardPosition;
+    private NavMeshAgent _Nav;
+
+    void Awake () {
+        _WizardPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        _Nav = GetComponent<NavMeshAgent>();
+    }
+	
+	void Update () {
+        _Nav.SetDestination(_WizardPosition);
+    }
+}
