b905e936fc75d5090d7ea56cb1ab896270236a53
Tymoteusz Machowski <tmachows@gmail.com>
2017-04-28 18:51:29 +0200

    make enemies of ice and lava and add proper animators

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -7,13 +7,22 @@ public class EnemyMover : MonoBehaviour {
 
     private Vector3 _WizardPosition;
     private NavMeshAgent _Nav;
+    private Animator _Animator;
 
     void Awake () {
         _Nav = GetComponent<NavMeshAgent>();
+        _Animator = GetComponent<Animator>();
     }
 	
 	void Update () {
-        _Nav.SetDestination(_WizardPosition);
+	    if (Vector3.Distance(transform.position, _WizardPosition) > 1.2)
+	    {
+	        _Nav.SetDestination(_WizardPosition);
+	    }
+	    else
+	    {
+	        _Animator.SetBool("CanReachHero", true);
+	    }
     }
 
     public void SetWizardPosition(Vector3 wizardPosition)
