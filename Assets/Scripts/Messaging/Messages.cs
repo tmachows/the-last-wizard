3e0cf320fd523d8a5a81f82146fbdaefbddb8a98
Tymoteusz Machowski <tmachows@gmail.com>
2017-05-18 22:02:25 +0200

    add messaging system

---
+++ Assets/Scripts/Messaging/Messages.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public static class MessageDispatcher
+{
+    public static void Send<T>(T message, IEnumerable<GameObject> objects) where T : class
+    {
+        foreach (var obj in objects)
+        {
+            InformGameObject(message, obj);
+        }
+        Break = false;
+    }
+
+    public static void Send<T>(T message, GameObject obj) where T : class
+    {
+        InformGameObject(message, obj);
+        Break = false;
+    }
+
+    public static void Send<T>(T message, Component component) where T : class
+    {
+        InformComponent(message, component);
+        Break = false;
+    }
+
+    public static bool Break = false;
+
+    private static void InformGameObject<T>(T message, GameObject obj) where T : class
+    {
+        var components = obj.GetComponents<Component>();
+        foreach (var component in components)
+        {
+            InformComponent(message, component);
+        }
+    }
+
+    private static void InformComponent<T>(T message, Component component) where T : class
+    {
+        var type = component.GetType();
+
+        var methods = type.GetMethods(
+            BindingFlags.Instance |    
+            BindingFlags.NonPublic |        
+            BindingFlags.Public |          
+            BindingFlags.FlattenHierarchy | 
+            BindingFlags.Static);          
+
+        foreach (var method in methods)
+        {
+            var parameters = method.GetParameters();
+
+            if (parameters.Length != 1)
+            {
+                continue;
+            }
+
+            var parameterType = parameters[0].ParameterType;
+
+            if (parameterType == message.GetType())
+            {
+                if (Break == true)
+                {
+                    return;
+                }
+
+                method.Invoke(component, new object[] { message });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Messaging/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class Utility
+{
+    public static IEnumerable<GameObject> OverlapSphere(Vector3 position, float radius,
+        bool attachedRigidbody = true)
+    {
+        return Physics.OverlapSphere(position, radius).Select(x =>
+        {
+            if (attachedRigidbody && x.attachedRigidbody != null)
+            {
+                return x.attachedRigidbody.gameObject;
+            }
+            return x.gameObject;
+        }
+        );
+    }
+}
